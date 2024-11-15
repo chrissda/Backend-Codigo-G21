ab923ef011d80af1f277c7c54e26bf4a2b34dd2a
chrissda <jhersonbarreto88@hotmail.com>
2024-11-14 21:03:45 -0500

    inicios en python

---
+++ dia_1/1_variables.py
@@ -0,0 +1,29 @@
+# Tipos de datos de variables primitivas
+# String
+nombre = 'Chriss'
+direccion = "Las Lechuzas '512"
+# Cuando queremos guardar un texto muy largo que ademas puede tener saltos de linea
+enunciado = '''Hola
+como estas
+        Te extraño
+Espero verte pronto,
+
+Saludos
+ 
+Chriss'''
+
+otro_enunciado = """Hola
+como estas
+        Te extraño
+Espero verte pronto,
+
+Saludos
+ 
+Chriss"""
+
+# Int
+edad = 32
+# Boolean
+casado = True
+# Float
+estatura = 1.77
