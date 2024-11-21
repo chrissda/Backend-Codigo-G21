5cbe7870e06da780eeb6ba95edd1022551a74eda
chrissda <jhersonbarreto88@hotmail.com>
2024-11-20 21:12:38 -0500

    postgres

---
+++ dia_1/introduccion.sql
@@ -0,0 +1,7 @@
+-- Asi se define un comentario en las bases de datos
+-- DDL (Data Definition Language) es un sublenguaje de SQL para definir como se almacenaran los datos
+CREATE DATABASE pruebas;
+
+
+-- Limpiara la terminal de nuesto psql
+\! cls
