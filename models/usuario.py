1038e09cd974ed1ffc5321390287de8f57878c93
chrissda <jhersonbarreto88@hotmail.com>
2024-12-11 23:02:50 -0500

    incios flask restful mysql

---
+++ models/usuario.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, types
+from instancias import conexion
+from enum import Enum
+
+class TipoUsuario(Enum):
+    # Creamos el enumerable sobre los posible valores que puede aceptar la columna
+    ADMIN = 'ADMIN'
+    CLIENTE = 'CLIENTE'
+    EMPLEADO = 'EMPLEADO'
+
+class Usuario(conexion.Model):
+    id = Column(type_=types.Integer, autoincrement=True, primary_key=True)
+    nombre = Column(type_=types.Text)
+    apellido = Column(type_=types.Text)
+    # En mysql no puede haber una columna que sea texto y unica
+    correo = Column(type_=types.String(100), unique=True, nullable=False)
+    password = Column(type_=types.Text)
+    tipo_usuario = Column(type_=types.Enum(TipoUsuario),
+                          default=TipoUsuario.CLIENTE)
+    
+    __tablename__ = 'usuarios'
+
+    # Si esta tabla sera creada y manejada en otra conexion que no es la predeterminada entonces usamos la variable __bnind_key__ para indicar que conexion debe usar
+    __bind_key__ = 'mysql'
