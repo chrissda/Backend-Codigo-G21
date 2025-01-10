2423be9cc0a0b13620754d9ebe3b2a3fa60bedc6
chrissda <jhersonbarreto88@hotmail.com>
2025-01-09 23:11:34 -0500

    Node express

---
+++ 2_usando_ecmascript/funciones.mjs
@@ -0,0 +1,16 @@
+// Arrow function (Funcion tipo flecha)
+// Funcion anonima
+export const sumar = (numero1, numero2) => {
+    const resultado = numero1 + numero2;
+    return resultado;
+}
+
+// Funcion tradicional
+export function restar (numero1, numero2) {
+    const resultado = numero1 - numero2;
+    return resultado;
+}
+
+// Adicional a ello si la funcio nes de una sola linea y retornara el resultado
+export const multiplicar = (numero1, numero2) => numero1 * numero2;
+
