466c5941cb61e5cfb22447c52cca80c6ecf7a6b0
chrissda <jhersonbarreto88@hotmail.com>
2025-01-24 17:53:39 -0500

    prueba de AWS

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-import express from 'express';
-import pg from 'pg';
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+const express = require("express");
+const pg = require("pg");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+// Definiendo los cors
+app.use(
+  cors({
+    methods: ["GET", "POST"], // '*'
+    origin: "*", //["http://mifrontend.com", "http://localhost:8080"], // '*'
+    allowedHeaders: ["Authorization", "Content-Type", "Accept"], // '*'
+  })
+);
 
+app.route("/").get((req, res) => {
+  return res.json({
+    content: process.env,
+    message: "Bienvenido a mi API",
+  });
+});
 const conexion = new pg.Pool({
+  // connectionString: `pg://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`,
   host: process.env.RDS_HOSTNAME,
   port: process.env.RDS_PORT,
   database: process.env.RDS_DB_NAME,
   user: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
+  ssl: true,
 });
 
 app
@@ -25,10 +43,10 @@ app
   .post(async (req, res) => {
     const data = req.body;
     const nuevoProducto = await conexion.query(
-      "INSERT INTO productos (nombre, precio) VALUES ($1, $2) RETURNING *",
+      "INSERT INTO productos (nombre, precio) VALUES ($1,$2) RETURNING *",
       [data.nombre, data.precio]
     );
-    
+
     const resultado = nuevoProducto.rows;
 
     return res.json({
@@ -47,5 +65,4 @@ app.listen(process.env.PORT, () => {
   ]).then(() => {
     console.log("Las tablas fueron creadas exitosamente");
   });
-
-}); 
+});
